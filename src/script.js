801e35f05a5370d22cccae880af830fbd8b16b37
Franciele Fernandes <franciele.fernandes@academico.ufpb.br>
2022-07-03 03:25:33 -0300

    ✨ feat: add menu

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const navMenu = document.getElementById("sidebar"),
+        navToggle = document.getElementById("nav-toggle"),
+        navClose = document.getElementById("nav-close")
+
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-sidebar")
+    })
+}
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-sidebar")
+    })
+}
